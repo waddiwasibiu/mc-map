f1f22abbf5e12ac63387e65588f1378b85a167cd
waddiwasibiu <waddiwasibiu@outlook.com>
2025-08-13 20:06:37 +0800

    LYX

--- script.js
+++ script.js
@@ -227,6 +227,7 @@ const defaultStructures = [
             { id: 2, x: 4386, y: 192, z: -3594, description: "地狱坐标" },
             { id: 3, x: 3680, y: 192, z: -8626, description: "地狱坐标" },
             { id: 4, x: 2929, y: 192, z: 3646, description: "地狱坐标" },
+            { id: 5, x: 5107, y: 192, z: 35, description: "地狱坐标" },
         ]
     }
 },
