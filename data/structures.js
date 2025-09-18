93b7ce2104ae9dd3a8137f16fa0f98bfd75cf21d
fancyi <58499077+ShenyiHu@users.noreply.github.com>
2025-09-19 00:48:25 +1000

    修改位置

--- data/structures.js
+++ data/structures.js
@@ -683,7 +683,7 @@ const defaultStructures = [
     coordinates: {
         server1: [
             { id: 1, x: 1592, y: 192, z: 4623, description: "天才粉毛甄选真坐标" },
-            { id: 2, x: 16560, y: 192, z: -6224, description: "真假未知 如验证可私聊粉毛" },
+            { id: 2, x: 16400, y: 192, z: -6400, description: "天才粉毛甄选真坐标" },
             { id: 3, x: -7977, y: 192, z: -18652, description: "真假未知 如验证可私聊粉毛" },
 
 
