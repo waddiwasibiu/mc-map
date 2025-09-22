bb94efd82a94570bdce9ea8ddfa66ef2b90fe455
fancyi <58499077+ShenyiHu@users.noreply.github.com>
2025-09-23 07:22:55 +1000

    Yan_chan

--- data/structures.js
+++ data/structures.js
@@ -37,6 +37,7 @@ const defaultStructures = [
             { id: 13, x: -5661, y: 192, z: 12136, description: "地狱坐标" },
             { id: 14, x: -6520, y: 192, z: 9935, description: "地狱坐标" },
             { id: 15, x: 5588, y: 192, z: -3884, description: "地狱坐标" },
+            { id: 16, x: -8070, y: 192, z: -10888, description: "地狱坐标" },
         ]
     }
 },
