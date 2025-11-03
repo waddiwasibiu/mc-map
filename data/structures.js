b7947db1a7cf4519c7d5720167c0ce309b9631f2
fancyi <58499077+ShenyiHu@users.noreply.github.com>
2025-11-03 22:11:01 +1100

    Fia

--- data/structures.js
+++ data/structures.js
@@ -714,6 +714,7 @@ const defaultStructures = [
             { id: 8, x: -1200, y: 192, z: 4320, description: "天才粉毛甄选真坐标" },
             { id: 9, x: -9329, y: 192, z: -20081, description: "菲亚甄选真坐标" },
             { id: 10, x: -21615, y: 192, z: -36573, description: "菲亚甄选真坐标" },
+            { id: 11, x: -37891, y: 192, z: -76070, description: "菲亚甄选真坐标" },
         ],
         server2: [
             { id: 1, x: -5419, y: 192, z: 4421, description: "天才粉毛甄选真坐标" },
